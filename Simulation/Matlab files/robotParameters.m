8be48ea96f1fc86c382cccffd0cb7618652d3eea
Kelvin Yankey <kelvinyankey6@gmail.com>
2022-08-26 14:38:30 +0200

    Added arduino rotation file

--- Simulation/Matlab files/robotParameters.m
+++ Simulation/Matlab files/robotParameters.m
@@ -5,4 +5,4 @@ ticksPerRot = 20; % Ticks per rotation for encoders
 load wheelLUT %Look up table for platform motors
 Ts = 0.01; %Sample time
 load wheelMotorModel %Motor model for platform motorswheelMotorModel
-load straigh_line.mat
+load straight_line.mat
